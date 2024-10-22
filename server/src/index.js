a3d11d9debc07cd142c4dfc34e04b55d5e29c149
EndzyCodes <127296867+ReginioMarkGilbert@users.noreply.github.com>
2024-10-22 15:10:50 +0800

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ const { ApolloServer } = require('apollo-server-express');
 const mongoose = require('mongoose');
 const { permitTypes, resolvePermitType } = require('./schema/permitTypes');
 const userResolvers = require('./resolvers/userResolvers');
+const adminResolvers = require('./resolvers/adminResolvers');
 const permitResolvers = require('./resolvers/permitResolvers/permitResolvers');
 const covResolvers = require('./resolvers/permitResolvers/covResolvers');
 const csawResolvers = require('./resolvers/permitResolvers/csawResolvers');
@@ -18,51 +19,52 @@ const app = express();
 
 // Middleware to parse JWT token
 const getUser = (token) => {
-  if (token) {
-    try {
-      return jwt.verify(token, process.env.JWT_SECRET);
-    } catch (err) {
-      throw new Error('Session invalid');
-    }
-  }
+   if (token) {
+      try {
+         return jwt.verify(token, process.env.JWT_SECRET);
+      } catch (err) {
+         throw new Error('Session invalid');
+      }
+   }
 };
 
 const server = new ApolloServer({
-  typeDefs: [userTypes, permitTypes],
-  resolvers: [
-    {
-      Permit: {
-        __resolveType: resolvePermitType
-      }
-    },
-    userResolvers,
-    permitResolvers,
-    covResolvers,
-    csawResolvers,
-    pltpResolvers,
-    ptprResolvers,
-    spltpResolvers,
-    tcebpResolvers
-  ],
-  context: ({ req }) => {
-    const token = req.headers.authorization || '';
-    const user = getUser(token.replace('Bearer ', ''));
-    return { user };
-  },
+   typeDefs: [userTypes, permitTypes],
+   resolvers: [
+      {
+         Permit: {
+            __resolveType: resolvePermitType
+         }
+      },
+      userResolvers,
+      adminResolvers,
+      permitResolvers,
+      covResolvers,
+      csawResolvers,
+      pltpResolvers,
+      ptprResolvers,
+      spltpResolvers,
+      tcebpResolvers
+   ],
+   context: ({ req }) => {
+      const token = req.headers.authorization || '';
+      const user = getUser(token.replace('Bearer ', ''));
+      return { user };
+   },
 });
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Could not connect to MongoDB', err));
+   .then(() => console.log('Connected to MongoDB'))
+   .catch(err => console.error('Could not connect to MongoDB', err));
 
 async function startServer() {
-  await server.start();
-  server.applyMiddleware({ app });
+   await server.start();
+   server.applyMiddleware({ app });
 
-  const PORT = process.env.PORT || 4000;
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}${server.graphqlPath}`);
-  });
+   const PORT = process.env.PORT || 4000;
+   app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}${server.graphqlPath}`);
+   });
 }
 
 startServer();
