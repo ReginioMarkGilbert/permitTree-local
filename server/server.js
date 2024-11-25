480214e699bc1db882b1c6e9251916af4621ff4a
EndzyCodes <127296867+ReginioMarkGilbert@users.noreply.github.com>
2024-11-25 11:56:07 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -56,6 +56,8 @@ const startServer = async () => {
    const corsOptions = {
       origin: [
          'http://localhost:5174',
+         'https://permittree-frontend-dev.vercel.app',
+         'https://permittree-backend-dev.vercel.app',
          'https://permittree-staging.vercel.app',
          'https://permittree-frontend.vercel.app',
          'https://permittree-backend.vercel.app'
